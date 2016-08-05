e59ba399c5d93fadb5518e139a9ab7dcbded306d
Hibari <49943231@gm.nfu.edu.tw>
2016-08-05 10:35:21 +0800

    20160805
    PHP&Mysql : select option use ajax
    下拉選單用Ajax 即時更改資料

---
+++ bradapi.php
@@ -0,0 +1,13 @@
+﻿<?php 
+	$db_host = 'localhost';
+	$db_user = 'root';
+	$db_pass = 'root';
+	$db_name = 'shop';
+	
+	$link = mysqli_connect($db_host, $db_user,
+					$db_pass, $db_name) 
+			or die("Server ERROR");
+	mysqli_query($link, "SET NAME 'UTF8'");
+//		
+
+?>
---
+++ heyjax.php
@@ -0,0 +1,27 @@
+﻿<?php
+include 'bradapi.php';
+$q = intval($_GET['q']);
+echo $q;
+// 搜尋 ajax table = option 的資料
+$sql="SELECT * FROM ajax WHERE id = $q ";
+$result = mysqli_query($link,$sql);
+
+echo "<table border='1'>
+    <tr>
+    <th>Name</th>    
+    <th>Age</th>    
+    </tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+    // 顯示資料
+    echo "<tr>";
+    echo "<td>" . $row['name'] . "</td>";
+    echo "<td>" . $row['age'] . "</td>";
+    echo "</tr>";
+ 
+}
+echo "</table>";
+
+
+?>
